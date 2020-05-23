c4c7cdb3c769ea467c917791b766e69391a9d14d
Sergio Augusto <sacbj@icomp.ufam.edu.br>
2020-05-23 11:05:14 -0400

    Migration do historico do paciente

---
+++ src/database/migrations/20200523085659_create_patient_history.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('patient_history', function (table){
+    table.string('cpf', 11).notNullable()
+    table.integer('score').notNullable()
+    table.timestamp('createdAt').defaultTo(knex.fn.now())
+
+    table.primary(['cpf', 'createdAt'])
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('patient_history');
+};
