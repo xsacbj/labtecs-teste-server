4a1f88819679c39c0c75a476ae6dede39f77bf3b
Sergio Augusto <sacbj@icomp.ufam.edu.br>
2020-05-23 13:03:08 -0400

    Adcionando campo de prioridade

--- src/database/migrations/20200523085659_create_patient_history.js
+++ src/database/migrations/20200523085659_create_patient_history.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('patient_history', function (table){
     table.string('cpf', 11).notNullable()
     table.integer('score').notNullable()
+    table.string('priority').notNullable()
     table.timestamp('createdAt').defaultTo(knex.fn.now())
 
     table.primary(['cpf', 'createdAt'])
